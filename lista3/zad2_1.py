4a18f57f9f0c2e4da2d48e167bfd28da69cab7bf
mkalitka <mikolaj.kalitka@proton.me>
2022-11-09 01:30:30 +0100

    Modified 3rd list

--- lista3/zad2_1.py
+++
@@ -1,3 +0,0 @@
-from zad2 import count_hiper_happy
-
-print(count_hiper_happy(8, 6))
--- lista3/zad4.py
+++ lista3/zad4.py
@@ -1,8 +1,32 @@
 import turtle
+from random import randint
 
 s = turtle.getscreen()
 t = turtle.Turtle()
 
-t.dot(600)
+t.speed(10)
+t.penup()
+t.backward(150)
+t.right(90)
+t.forward(75)
+t.left(180)
+
+height = 20
+
+for i in range(40):
+    t.pendown()
+    t.forward(height)
+    t.right(90)
+    t.forward(5)
+    t.right(90)
+    t.forward(height)
+    t.right(90)
+    t.forward(5)
+    t.right(180)
+    t.penup()
+    t.forward(7)
+    t.left(90)
+    t.penup()
+    height += randint(0, 4)
 
 turtle.mainloop()
